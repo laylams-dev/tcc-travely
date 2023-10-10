544874843ff1f4e8c12a148b0aed4a6ff49d4b7a
Layla Miranda <layla.miranda@aluna.ufop.edu.br>
2023-10-10 00:13:42 -0300

    fix(backend): Remove comentário criado pelo Adonis na migration de usuário

--- travely-backend/database/migrations/1695857334482_users.ts
+++ travely-backend/database/migrations/1695857334482_users.ts
@@ -10,10 +10,6 @@ export default class extends BaseSchema {
       table.string('password', 180).notNullable()
       table.string('role', 5).notNullable().defaultTo('user')
       table.string('remember_me_token').nullable()
-
-      /**
-       * Uses timestampz for PostgreSQL and DATETIME2 for MSSQL
-       */
       table.timestamp('created_at', { useTz: true }).notNullable()
       table.timestamp('updated_at', { useTz: true }).notNullable()
     })
